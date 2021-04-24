b957a2561170a8f26b1007d9b36a7f2edee8cc9a
trackmystories <ashirazee@gmail.com>
2021-04-24 09:14:17 +0500

    Update test files

--- src/components/__tests__/map-test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Map from '../map';
-import renderer from 'react-test-renderer';
-
-describe('Map', () => {
-  it('renders without error', () => {
-    renderer.create(<Map />);
-  });
-});
